de6b392c5d72da553da3e0efb2bd2bfa15d87cac
Anarchasm <53111844+Anarchasm@users.noreply.github.com>
2020-06-21 20:05:17 -0400

    summer

--- itemlists.js
+++ itemlists.js
@@ -1,6 +1,6 @@
 var fest = [
-	{name: "Spring Shortcake", dathumb: ":thumb734910909:", url: "Spring-Shortcake-734910909"},
-	{name: "Cherry Blossom", dathumb: ":thumb734115289:", url: "Cherry-Blossom-734115289"},
+	{name: "Summer Pop", dathumb: ":thumb734910909:", url: "Summer-Pop-747787514"},
+	{name: "Conch Shell", dathumb: ":thumb734115289:", url: "Conch-Shell-750481154"},
 	{name: "Pride Blend", dathumb: ":thumb747807943:", url: "Pride-Blend-747807943"},
 	];
 	
@@ -10,6 +10,7 @@ var companions = [
 	{name: "Tunneldrake Companion", dathumb: ":thumb747809073:", url: "Tunneldrake-Companion-747809073"},
 	{name: "Mymber Companion", dathumb: ":thumb756147760:", url: "Mymber-Companion-756147760"},
 	{name: "Kellas Cat Companion", dathumb: ":thumb768254398:", url: "Kellas-Cat-Companion-768254398"}
+	{name: "Rooter Companion", dathumb: ":thumb787307888:", url: "Rooter-Companion-787307888", quantity: "low", contraband: false}
 ];
 
 var cats = [
