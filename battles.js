27b2ead1c32e6f53f02de9dff2949d28f885e475
pawstepsinthesnow <39842038+pawstepsinthesnow@users.noreply.github.com>
2025-04-13 12:50:52 -0500

    refresh, stashing wip. nonfunctional.

---
+++ battles.js
@@ -0,0 +1,35 @@
+var alpha = { //Holding object for Player 1's data
+	strength: 0,
+	dexterity: 0,
+	stunned: false,
+	haste: false,
+	weakened: false,
+	muteki: false,
+	speedrush: false,
+	glide: false,
+	reflect: false,
+	confuse: false,
+	stopped: false,
+	cauterize: false,
+	secondwind: false,
+	nescape: false,
+	rage: false
+};
+var bravo = { //Holding object for Player 2's data (The opponent in KvK or fauna)
+	strength: 0,
+	dexterity: 0,
+	stunned: false,
+	haste: false,
+	weakened: false,
+	muteki: false,
+	speedrush: false,
+	glide: false,
+	reflect: false,
+	confuse: false,
+	stopped: false,
+	cauterize: false,
+	secondwind: false,
+	nescape: false,
+	rage: false
+};
+
---
+++ skills.js
@@ -0,0 +1,6 @@
+var skills = [
+	{name:"Arc Flash", power:80, accuracy:250, effect:"none", chance:0, attunement:true},
+	{name:"Crush", power:40, accuracy:350, effect:"none", chance:0, attunement:true},
+	{name:"Haste", power:0, accuracy:300, effect:"haste", chance:100, attunement:true},
+	{name:"Slam", power:5, accuracy:350, effect:"stun", chance:50, attunement:false}
+	];
